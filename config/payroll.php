d6d21eb479f879b04cd1036855d5fef2731244bc
Nasrul Hazim <nasrulhazim.m@gmail.com>
2018-08-14 17:01:28 +0800

    Restructure Config File

--- config/payroll.php
+++ config/payroll.php
@@ -2,28 +2,40 @@
 
 
 return [
-    'deduction_types' => [
+    'seeds' => [
+        'deduction_types' => [
         'Loan',
         'Income Tax',
-    ],
-    'earning_types' => [
-        'Basic',
-        'Overtime',
-        'Allowance',
-        'Bonus',
-        'Other',
+        ],
+        'earning_types' => [
+            'Basic',
+            'Overtime',
+            'Allowance',
+            'Bonus',
+            'Other',
+        ],
+        'payroll_statuses' => [
+            'Active', 'Inactive', 'Locked',
+        ],
+        'payslip_statuses' => [
+            'Active', 'Inactive', 'Locked',
+        ],
     ],
     'models' => [
-        'user'           => \App\User::class,
-        'payroll'        => \CleaniqueCoders\OpenPayroll\Models\Payroll::class,
-        'payroll_status' => \CleaniqueCoders\OpenPayroll\Models\Payroll\Status::class,
-        'payslip'        => \CleaniqueCoders\OpenPayroll\Models\Payslip::class,
+        'user'            => \App\User::class,
+        'payroll'         => \CleaniqueCoders\OpenPayroll\Models\Payroll\Payroll::class,
+        'payroll_status'  => \CleaniqueCoders\OpenPayroll\Models\Payroll\Status::class,
+        'payslip'         => \CleaniqueCoders\OpenPayroll\Models\Payslip\Payslip::class,
+        'payslip_status'  => \CleaniqueCoders\OpenPayroll\Models\Payslip\Status::class,
+        'deduction'       => \CleaniqueCoders\OpenPayroll\Models\Deduction\Deduction::class,
+        'deduction_types' => \CleaniqueCoders\OpenPayroll\Models\Deduction\Status::class,
+        'earning'         => \CleaniqueCoders\OpenPayroll\Models\Earning\Earning::class,
+        'earning_types'   => \CleaniqueCoders\OpenPayroll\Models\Earning\Status::class,
     ],
     'tables' => [
-        'prefix' => 'op_',
-        'names'  => [
-            'payslip_earnings',
-            'payslip_deductions',
+        'names' => [
+            'earnings',
+            'deductions',
             'payslips',
             'payrolls',
             'payroll_statuses',
