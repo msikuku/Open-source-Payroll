069b422d676b24d1c43ed3402145446c2648d7f4
Nasrul Hazim <nasrulhazim.m@gmail.com>
2018-08-14 21:14:12 +0800

    Added references data test

--- tests/PayrollFactoryTest.php
+++ tests/PayrollFactoryTest.php
@@ -3,18 +3,38 @@
 namespace CleaniqueCoders\OpenPayroll\Tests;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Str;
 
 class PayrollFactoryTest extends TestCase
 {
     use RefreshDatabase;
 
+    /** @test */
     public function it_has_seeders()
     {
-        $this->assertFileExists(database_path('seeds/PayrollSeeder.php'));
-        $this->assertFileExists(database_path('seeds/PayrollTestSeeder.php'));
+        $this->assertHasClass('PayrollSeeder');
+        $this->assertHasClass('PayrollTestSeeder');
     }
 
-    public function it_can_seed_users()
+    /** @test */
+    public function it_has_references_data()
     {
+        $this->artisan('db:seed', ['--class' => 'PayrollSeeder']);
+        $tables = config('open-payroll.tables.names');
+
+        foreach ($tables as $table) {
+            $this->assertHasTable($table);
+        }
+
+        $seeds = config('open-payroll.seeds');
+
+        foreach ($seeds as $table => $data) {
+            foreach ($data as $datum) {
+                $this->assertDatabaseHas($table, [
+                    'name' => $datum,
+                    'code' => Str::kebab($datum),
+                ]);
+            }
+        }
     }
 }
