9300e7241ee743bad410783b96f726ae411f9996
Nasrul Hazim <nasrulhazim.m@gmail.com>
2018-08-19 16:45:50 +0800

    Update Open Payroll Factory

--- database/factories/OpenPayrollFactory.php
+++ database/factories/OpenPayrollFactory.php
@@ -5,6 +5,9 @@ use Faker\Generator as Faker;
 
 $factory->define(\App\Models\OpenPayroll\Employee::class, function(Faker $faker) {
     return [
+        'name' => $faker->name,
+        'email' => $faker->unique()->email,
+        'password' => bcrypt('password'),
     ];
 });
 
