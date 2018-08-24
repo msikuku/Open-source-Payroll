20ee0221fc9b686b401a76a9cb4a65a64175c156
Nasrul Hazim <nasrulhazim.m@gmail.com>
2018-08-24 11:57:25 +0800

    Test: Update User Stub

--- tests/Stubs/User.php
+++ tests/Stubs/User.php
@@ -2,8 +2,12 @@
 
 namespace CleaniqueCoders\OpenPayroll\Tests\Stubs;
 
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
+	use Notifiable;
+	
+	protected $guarded = ['id'];
 }
